a5dc7a14b5d29121866b55d18bd64e4a017fb061
Marshall Buck <dev@marshallbuck.com>
2022-09-22 16:19:26 -0500

    working on shortestpath

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -18,26 +18,26 @@ class Graph {
   /** add Node instance and add it to nodes property on graph. */
   addVertex(vertex) {
     this.nodes.add(vertex);
-   }
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
   addVertices(vertexArray) {
-    for(let vertex of vertexArray){
+    for (let vertex of vertexArray) {
       this.nodes.add(vertex);
     }
-   }
+  }
 
   /** add edge between vertices v1,v2 */
   addEdge(v1, v2) {
     v1.adjacent.add(v2);
     v2.adjacent.add(v1);
-   }
+  }
 
   /** remove edge between vertices v1,v2 */
   removeEdge(v1, v2) {
     v1.adjacent.delete(v2);
     v2.adjacent.delete(v1);
-   }
+  }
 
   /** remove vertex from graph:
    *
@@ -45,11 +45,11 @@ class Graph {
    * - update any adjacency lists using that vertex
    */
   removeVertex(vertex) {
-    for(let adj of vertex.adjacent){
+    for (let adj of vertex.adjacent) {
       this.removeEdge(vertex, adj);
     }
     this.nodes.delete(vertex);
-   }
+  }
 
   /** traverse graph with DFS and returns array of Node values */
   depthFirstSearch(start) {
@@ -57,33 +57,12 @@ class Graph {
     let visited = new Set(stack);
     let seen = [];
 
-    while(stack.length > 0){
+    while (stack.length > 0) {
       let currVertex = stack.pop();
       seen.push(currVertex.value);
 
-      for(let neighbor of currVertex.adjacent){
-        if(!visited.has(neighbor)){
-          stack.push(neighbor);
-          visited.add(neighbor);
-        }
-      }
-    }
-
-    return seen;
-   }
-
-  /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) {
-    let stack = [start];
-    let visited = new Set(stack);
-    let seen = [];
-
-    while(stack.length > 0){
-      let currVertex = stack.shift();
-      seen.push(currVertex.value);
-
-      for(let neighbor of currVertex.adjacent){
-        if(!visited.has(neighbor)){
+      for (let neighbor of currVertex.adjacent) {
+        if (!visited.has(neighbor)) {
           stack.push(neighbor);
           visited.add(neighbor);
         }
@@ -93,20 +72,42 @@ class Graph {
     return seen;
   }
 
+  /** traverse graph with BDS and returns array of Node values */
+  breadthFirstSearch(start) {
+    let queue = [start];
+    let visited = new Set(queue);
+    let seen = [];
+
+    while (queue.length > 0) {
+      let currVertex = queue.shift();
+      seen.push(currVertex.value);
+
+      for (let neighbor of currVertex.adjacent) {
+        if (!visited.has(neighbor)) {
+          queue.push(neighbor);
+          visited.add(neighbor);
+        }
+      }
+    }
+
+    return seen;
+  }
+
   /** find the distance of the shortest path from the start vertex to the end vertex */
-  distanceOfShortestPath(start, end, minDistance = 0) {
+  distanceOfShortestPath(start, end, count = 0, seen = new Set([start])) {
 
-    let minDistance = 0;
-    let firstSeen = [];
-    let stack = [start];
+    if (start === end) return count;
 
-    if()
+    for (const neighbor of start.adjacent) {
+      if (!seen.has(neighbor)) {
+        seen.add(neighbor);
+        count++;
 
-
-
-
-
-   }
+      } return this.distanceOfShortestPath(neighbor, count, seen);
+    }
+    return count;
+  }
 }
 
-module.exports = { Graph, Node }
+
+module.exports = { Graph, Node };
